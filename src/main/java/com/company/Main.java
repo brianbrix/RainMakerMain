bc28cf1f55a0d7120f4ee1a6b33e27454584a7b3
brianbrix <mokandubrian@gmail.com>
2022-05-16 10:41:54 +0300

    Commit

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,14 +1,10 @@
 package com.company;
 
 
-import org.eclipse.jgit.api.errors.GitAPIException;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
 
 public class Main {
 
-    public static void main(String[] args) throws GitAPIException, IOException, URISyntaxException {
+    public static void main(String[] args) {
         ClipboardTextListener b = new ClipboardTextListener();
         Thread thread = new Thread(b);
         thread.start();
