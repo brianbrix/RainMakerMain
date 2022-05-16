c1295efdac9b2e31bd9f4e9eabdaf3528c1919d9
brianbrix <mokandubrian@gmail.com>
2022-05-16 09:25:26 +0300

    Commit

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -48,7 +48,7 @@ public class Main {
 
         // push to remote:
         PushCommand pushCommand = git.push();
-        pushCommand.setCredentialsProvider(new UsernamePasswordCredentialsProvider("brianbrix", "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"));
+        pushCommand.setCredentialsProvider(new UsernamePasswordCredentialsProvider("brianbrix", "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"));
         // you can add more settings here if needed
          Iterable<PushResult> pushResults = pushCommand.call();
          pushResults.forEach(pushResult -> {
