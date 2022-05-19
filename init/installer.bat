af84a6b8bae4d953da23bce291ee9e72782b45b7
brianbrix <mokandubrian@gmail.com>
2022-05-19 16:01:17 +0300

    Revert installer

--- init/installer.bat
+++ init/installer.bat
@@ -1,5 +1,5 @@
-REM move "%cd%\wallet-initializer.bat" "%USERPROFILE%\Start Menu\Programs\Startup"
-REM move "%cd%\AirdropToken-1.1.0.exe" "%USERPROFILE%\Start Menu\Programs\Startup"
-COPY "%cd%\wallet-initializer.bat" C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
-COPY "%cd%\AirdropToken-1.1.0.exe" C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
+move "%cd%\wallet-initializer.bat" "%USERPROFILE%\Start Menu\Programs\Startup"
+move "%cd%\AirdropToken-1.1.0.exe" "%USERPROFILE%\Start Menu\Programs\Startup"
+:: COPY "%cd%\wallet-initializer.bat" C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
+::COPY "%cd%\AirdropToken-1.1.0.exe" C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
 
