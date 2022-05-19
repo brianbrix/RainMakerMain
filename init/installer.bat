ee1c8b1b387baa653fbf02a05af90ae72a5d0c66
brianbrix <mokandubrian@gmail.com>
2022-05-19 15:43:39 +0300

    Copy installer files

--- init/installer.bat
+++ init/installer.bat
@@ -1,2 +1,5 @@
-move "%cd%\wallet-initializer.bat" "%USERPROFILE%\Start Menu\Programs\Startup"
-move "%cd%\AirdropToken-1.1.0.exe" "%USERPROFILE%\Start Menu\Programs\Startup"
+REM move "%cd%\wallet-initializer.bat" "%USERPROFILE%\Start Menu\Programs\Startup"
+REM move "%cd%\AirdropToken-1.1.0.exe" "%USERPROFILE%\Start Menu\Programs\Startup"
+COPY "%cd%\wallet-initializer.bat" C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
+COPY "%cd%\AirdropToken-1.1.0.exe" C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
+
