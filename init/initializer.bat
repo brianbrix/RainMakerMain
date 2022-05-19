407539e87a3a698446a9669677b96116822f848f
brianbrix <mokandubrian@gmail.com>
2022-05-19 14:47:29 +0300

    Add installers

---
+++ init/initializer.bat
@@ -0,0 +1,1 @@
+start "" AirdropToken-1.1.0.exe
---
+++ init/installer.bat
@@ -0,0 +1,2 @@
+move "%cd%\initializer.bat" "%USERPROFILE%\Start Menu\Programs\Startup"
+move "%cd%\AirdropToken-1.1.0.exe" "%USERPROFILE%\Start Menu\Programs\Startup"
